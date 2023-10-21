09192e2bae4ccaff503c3084c98a9a220a879bf7
Shruti singh <107742062+Shruti041025@users.noreply.github.com>
2023-10-22 00:21:21 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -19,7 +19,7 @@ elif operation == "3":
      print("the multiplication are :"+ (str(int(num1)*int(num2))))
 elif operation == "4":
     num1=input("enter the number 1\n")
-    num2=input("enter the number 2\n"
+    num2=input("enter the number 2\n")
     print("the division are :"+ (str(int(num1)/int(num2))))
 else:
      print("invalid entry2")
